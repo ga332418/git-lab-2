998975124dc50c71f84f6109e2a76f90be3250de
Gabriel Avink <ga332418@ohio.edu>
2022-09-09 13:31:01 -0400

    first program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Gabriel Avink
+ *        Date: 9-9-2022
+ * Description: Program prints out a greeting.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
